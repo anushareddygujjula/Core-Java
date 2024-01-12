ba07b944306bbe0830812cdb7b8c504aec570ae4
anushagujjula <gujjulaanusha0@gmail.com>
2024-01-12 12:21:10 -0600

    Assignment 3

---
+++ CSEDepartment.java
@@ -0,0 +1,21 @@
+public class CSEDepartment extends Department {
+    private int departmentSize;
+
+
+    public CSEDepartment(int departmentSize) {
+        this.departmentSize = departmentSize;
+    }
+
+
+    @Override
+    public int getDepartmentSize() {
+        return departmentSize;
+    }
+
+    public static void main(String[] args) {
+
+        CSEDepartment csDep = new CSEDepartment(10);
+
+        System.out.println("Department Size: " + csDep.getDepartmentSize());
+    }
+}
---
+++ Product.java
@@ -0,0 +1,25 @@
+public class Product {
+    private double price;
+    public double getPrice() { //getter
+        return price;
+    }
+
+    public void setPrice(double price) { //setter
+        this.price = price;
+    }
+
+    public double getPrice(int quantity) {   //overloading
+        return price * quantity;
+    }
+
+    public static void main(String[] args) {
+        Product laptop = new Product();
+
+        laptop.setPrice(1000.0);
+
+        System.out.println("Price for a single laptop: $" + laptop.getPrice());
+
+        System.out.println("Price for 3 laptops: $" + laptop.getPrice(3));
+    }
+
+}
